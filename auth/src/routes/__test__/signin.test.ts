5c535bd87e3855e482c758e577950e809ef776f7
hnmishra <mishrahn@gmail.com>
2025-03-07 10:56:12 +0530

    updated signin.tst

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -46,5 +46,5 @@ it('responds with a cookie when given valid credentials', async () => {
         })
         .expect(200);
 
-    expect(response.get('Set-Cookie')).toBeTruthy();
+    expect(response.get('Set-Cookie')).toBeDefined();
 });
