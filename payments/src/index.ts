24d39a09e38844cbec5828387366e9826e6da8df
hnmishra <mishrahn@gmail.com>
2025-03-09 13:04:23 +0530

    Added comments to test the depls

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -6,6 +6,7 @@ import { OrderCancelledListener } from "./events/listeners/order-cancelled-liste
 
 //connect to mongo db
 const start = async () => {
+    console.log('Starting payments...');
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
     }
